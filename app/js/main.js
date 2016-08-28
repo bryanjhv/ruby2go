46b12574ac8383e9bdcf5ac9e992c80d2106760b
Bryan Horna <bryanjhv@gmail.com>
2016-08-28 02:21:12 -0500

    Join code editor with code execution

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,57 @@
     btnRun.disabled = editor.getValue().trim() == '';
   });
 
+  var ul = document.getElementById('history');
+  var active = document.querySelector('.active');
+
+  // Run code on btnRun click
+  btnRun.addEventListener('click', function () {
+    var code = editor.getValue().trim(),
+      date = new Date();
+
+    var self = this;
+    self.disabled = true;
+
+    if (active) {
+      active.className = '';
+    }
+
+    var progress = document.createElement('progress'),
+      parent = ul.parentNode;
+    progress.value = 0;
+    progress.max = 100;
+    parent.appendChild(progress);
+
+    run(code, function (error, result) {
+      var li = document.createElement('li'), p;
+      li.className = 'active';
+
+      p = document.createElement('p');
+      li.appendChild(p);
+      p.innerText = result || ' '; // fix empty output
+      if (error) {
+        p.className = 'error';
+        p.innerText = error.name + ': ' + error.message;
+      }
+
+      p = document.createElement('p');
+      li.appendChild(p);
+      var time = document.createElement('time');
+      time.dateTime = date.toJSON();
+      time.innerText = date.toString().slice(16, -12);
+      p.appendChild(time);
+      p.insertAdjacentText('beforeend', code);
+
+      if (active) {
+        ul.insertBefore(li, active);
+      } else {
+        ul.appendChild(li);
+      }
+      active = li;
+
+      parent.removeChild(progress);
+      self.disabled = false;
+    });
+  }, false);
+
 })();
